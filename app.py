21347a119e30913ff040bdd00a3c36c2c9c73466
Robin Le Floch <77750420+nonfinito@users.noreply.github.com>
2024-05-31 11:18:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ import requests
 
 st.title('NY TAXI: IS THAT A FAIR FARE?')
 
-st.image('taxi.jpg', caption='NY TAXI', use_column_width=True)
+st.image('taxi.jpg', caption='Do you want to see my big 🍏?', use_column_width=True)
 
 # '''
 # ## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
